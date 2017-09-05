316c96274acf00c50e31a01ed4cf060da0860d53
chai2010 <chaishushan@gmail.com>
2017-09-05 10:19:42 +0800

    spec: 更多的接口文件

--- spec.pb/a_gen_proto.go
+++ spec.pb/a_gen_proto.go
@@ -5,8 +5,8 @@
 // https://github.com/google/protobuf/releases
 // go install github.com/chai2010/qingcloud-go/protoc-gen-go
 
-// protoc --go_out=plugins=qingcloud:. base.proto instances.proto
+// +build !windows
 
-//go:generate protoc --go_out=plugins=qingcloud:. base.proto instances.proto nic.proto snapshot.proto zone.proto cache.proto             job.proto               rdb.proto               tag.proto dns.proto               key_pair.proto          router.proto            user_data.proto eip.proto               load_balancer.proto     security_group.proto    volume.proto image.proto             mongo.proto             shared_storage.proto    vxnet.proto
+//go:generate make
 
 package spec
