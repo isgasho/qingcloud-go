e80231fe977938c4624e37ed936edbf0d72f76dd
chai2010 <chaishushan@gmail.com>
2017-09-05 08:36:01 +0800

    protoc-gen-go: 增加对扩展语言的设计思路

---
+++ protoc-gen-go/link_qingcloud_unknown.go
@@ -0,0 +1,9 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package main
+
+// protoc --go_out=plugins=qingcloud:tmpl=./tmpl-dir:. xxx.proto
+
+// read mustache template for xlang.
