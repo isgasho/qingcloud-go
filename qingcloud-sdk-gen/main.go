be569ade214384ba1a9f6543ae44f63ce4d976e3
chai2010 <chaishushan@gmail.com>
2017-09-05 10:23:11 +0800

    qingcloud-sdk-gen: 增加包说明

--- qingcloud-sdk-gen/main.go
+++ qingcloud-sdk-gen/main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a Apache
 // license that can be found in the LICENSE file.
 
+// qingcloud-gen-go is a code generator for QingCloud SDK.
 package main
 
 func main() {
