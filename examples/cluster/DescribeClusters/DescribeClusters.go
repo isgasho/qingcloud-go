0a59d051137219e1c764a4173631c42711210e70
chai2010 <chaishushan@gmail.com>
2017-09-06 21:26:42 +0800

    整理例子

---
+++ examples/cluster/DescribeClusters/DescribeClusters.go
@@ -0,0 +1,58 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	qcConfig "github.com/yunify/qingcloud-sdk-go/config"
+	qcSservice "github.com/yunify/qingcloud-sdk-go/service"
+)
+
+func main() {
+	conf := loadUserConfig()
+	conf.LogLevel = "debug"
+
+	service, err := qcSservice.Init(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clusterService, err := service.Cluster("pek3a")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reply, err := clusterService.DescribeClusters(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(encodeJSON(reply)))
+}
+
+func loadUserConfig() *qcConfig.Config {
+	conf, err := qcConfig.NewDefault()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = conf.LoadUserConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conf
+}
+
+func encodeJSON(m interface{}) []byte {
+	data, err := json.MarshalIndent(m, "", "\t")
+	if err != nil {
+		return nil
+	}
+	data = bytes.Replace(data, []byte("\n"), []byte("\r\n"), -1)
+	return data
+}
