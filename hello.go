344e02903f9cb35a86ada6dc099d7b46df675c03
chai2010 <chaishushan@gmail.com>
2017-09-07 02:19:03 +0800

    hello: 验证 proto 到 map 的转化, 技术可行

--- hello.go
+++ hello.go
@@ -10,6 +10,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+
+	pb "github.com/chai2010/qingcloud-go/spec.pb"
+	"github.com/golang/protobuf/jsonpb"
 )
 
 // 以前成员全部是定义为指针类型
@@ -28,6 +32,50 @@ import (
 // var requestParams map[string]string
 
 func main() {
+	reqInput := &pb.DescribeNicsInput{
+		Instances: []string{"Instances-1", "Instances-2"},
+		Limit:     12,
+		NicName:   "NicName-111",
+		Nics:      []string{"Nics-111", "Nics-222"},
+		Offset:    123,
+		Status:    "ok",
+		VxnetType: 2,
+		Vxnets:    []string{"Vxnets-11", "Vxnets-22"},
+	}
+
+	jsonMarshaler := &jsonpb.Marshaler{
+		OrigName: true,
+		Indent:   "  ",
+	}
+
+	s, err := jsonMarshaler.MarshalToString(reqInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("json:", s)
+
+	var dat map[string]interface{}
+	if err := json.Unmarshal([]byte(s), &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
+
+	num := dat["limit"].(float64)
+	fmt.Println("limit:", num)
+
+	fmt.Println("nic_name:", dat["nic_name"].(string))
+
+	switch dat["nic_name"].(type) {
+	case string:
+		fmt.Println("nic_name type:", "string")
+	}
+
+	fmt.Println("vxnets[0]:", dat["vxnets"].([]interface{})[0].(string))
+	fmt.Println("vxnets[1]:", dat["vxnets"].([]interface{})[1].(string))
+}
+
+func main2() {
 	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
 
 	var dat map[string]interface{}
