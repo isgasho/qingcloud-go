a116e19bd3e5e8e5f3088207be8887504a331bda
chai2010 <chaishushan@gmail.com>
2017-09-06 21:38:01 +0800

    根目录增加版本信息

--- doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
-// Use of this source code is governed by a Apache
-// license that can be found in the LICENSE file.
-
-// Package qingcloud provides QingCloud SDK for Go.
-package qingcloud
--- version.go
+++ version.go
@@ -2,4 +2,10 @@
 // Use of this source code is governed by a Apache
 // license that can be found in the LICENSE file.
 
+// Package qingcloud is the non official QingCloud SDK for the Go programming language.
+//
+// https://github.com/chai2010/qingcloud-go
 package qingcloud
+
+// Version number.
+const Version = "0.0.1"
