fc4cd2a24e40f1bb72ed08b3194563623955c6bd
chai2010 <chaishushan@gmail.com>
2017-09-13 22:58:51 +0800

    protoc-gen-go: 完善模板

--- protoc-gen-go/qingcloud/tmpl.go
+++ protoc-gen-go/qingcloud/tmpl.go
@@ -4,33 +4,91 @@
 
 package qingcloud
 
-import (
-	rule_pb "github.com/chai2010/qingcloud-go/spec.pb/qingcloud_sdk_rule"
-)
-
 type ServiceSpec struct {
+	DocUrl          string
+	ServiceName     string
 	MainServiceName string
-	SubServiceName  string
 	MethodList      []MethodSpec
-	*rule_pb.ServiceOptionsRule
 }
 
 type MethodSpec struct {
+	DocUrl         string
 	MethodName     string
 	InputTypeName  string
 	OutputTypeName string
-	*rule_pb.MethodOptionsRule
+	HttpMethod     string
 }
 
 type MessageSpec struct {
 	MessageTypeName string
-	*rule_pb.MessageOptionsRule
+	RequiredFileds  []string
+	DefaultValue    map[string]string
+	EnumValue       map[string][]string
+	MinValue        map[string]string
+	MaxValue        map[string]string
+	MultipleOfValue map[string]string
+	RegexpValue     map[string]string
 }
 
 const tmplService = `
-{{/* TODO */}}
+
+{{if .DocUrl}}// See {{.DocUrl}}{{end}}
+type {{.ServiceName}}Service struct {
+	Config           *config.Config
+	Properties       *{{.ServiceName}}Properties
+	LastResponseBody string
+}
+
+func New{{.ServiceName}}Service(conf *config.Config, zone string) (p *{{.ServiceName}}Service) {
+	return &{{.ServiceName}}Service{
+		Config:     conf,
+		Properties: &{{.ServiceName}}Properties{Zone: zone},
+	}
+}
+
+{{if not .MainServiceName}}
+func (s *{{.MainServiceName}}Service) {{.ServiceName}}(zone string) (*{{.ServiceName}}Service, error) {
+	properties := &{{.ServiceName}}ServiceProperties{
+		Zone: zone,
+	}
+
+	return &{{.ServiceName}}{Config: s.Config, Properties: properties}, nil
+}
+{{end}}
+
+{{range $_, $m := .MethodList}}
+{{if .DocUrl}}// See {{.DocUrl}}{{end}}
+func (p *{{.ServiceName}}Service) {{$m.MethodName}}(in *{{$m.InputTypeName}}) (out *{{$m.OutputTypeName}}, err error) {
+	if in == nil {
+		in = &{{$m.InputTypeName}}{}
+	}
+	o := &data.Operation{
+		Config:        p.Config,
+		Properties:    p.Properties,
+		APIName:       "{{$m.MethodName}}",
+		RequestMethod: "{{$m.HttpMethod}}",
+	}
+
+	x := &{{$m.OutputTypeName}}{}
+	r, err := request.New(o, in, x)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.Send()
+	p.LastResponseBody = o.ResponseBody
+
+	if err != nil {
+		return nil, err
+	}
+
+	return x, err
+}
+{{end}}
 `
 
 const tmplMessageValidate = `
-{{/* TODO */}}
+func (p *{{.MessageTypeName}}) Validate() error {
+	return nil
+}
 `
