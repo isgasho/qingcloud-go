170cc3724ea1aa037c3ec836b79f49b6f5f269a0
chai2010 <chaishushan@gmail.com>
2017-09-13 21:58:32 +0800

    protoc-gen-go: 删除不需要的代码

--- protoc-gen-go/link_qingcloud_unknown.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
-// Use of this source code is governed by a Apache
-// license that can be found in the LICENSE file.
-
-package main
-
-// protoc --go_out=plugins=qingcloud:tmpl=./tmpl-dir:. xxx.proto
-
-// read mustache template for xlang.
