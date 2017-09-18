d93bd8a1b6bffad6fed059c746e81ae75508e47d
chai2010 <chaishushan@gmail.com>
2017-09-18 15:47:36 +0800

    examples: 添加集群启动和停止的例子

---
+++ examples/cluster/StopClusters/StopClusters.go
@@ -0,0 +1,58 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/chai2010/qingcloud-go/config"
+	pb "github.com/chai2010/qingcloud-go/service"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	flag.Parse()
+
+	conf := config.MustLoadUserConfig()
+	conf.JSONDisableUnknownFields = false
+	conf.LogLevel = "debug" // debug/warn
+
+	qcService, err := pb.Init(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clusterService, err := qcService.Cluster("pek3a")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reply, err := clusterService.StopClusters(&pb.StopClustersInput{
+		Clusters: []string{"cl-hvyhfuwl"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(jsonpbEncode(reply))
+}
+
+func jsonpbEncode(m proto.Message) string {
+	jsonMarshaler := &jsonpb.Marshaler{
+		OrigName:     true,
+		Indent:       "  ",
+		EnumsAsInts:  true,
+		EmitDefaults: true,
+	}
+
+	s, err := jsonMarshaler.MarshalToString(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s
+}
