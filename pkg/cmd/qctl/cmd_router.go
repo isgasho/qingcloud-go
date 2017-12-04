ac46b5dad71f7e50ed13ef08da33f0a32934aaf2
chai2010 <chaishushan@gmail.com>
2017-12-04 15:15:02 +0800

    qctl: 完善命令行

---
+++ pkg/cmd/qctl/cmd_router.go
@@ -0,0 +1,35 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package qctl
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+var cmdRouter = cli.Command{
+	Name:    "router",
+	Aliases: []string{},
+	Usage:   "manage router",
+	Subcommands: []cli.Command{
+		{
+			Name:  "add",
+			Usage: "add a new template",
+			Action: func(c *cli.Context) error {
+				fmt.Println("new task template: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:  "remove",
+			Usage: "remove an existing template",
+			Action: func(c *cli.Context) error {
+				fmt.Println("removed task template: ", c.Args().First())
+				return nil
+			},
+		},
+	},
+}
--- pkg/cmd/qctl/main.go
+++ pkg/cmd/qctl/main.go
@@ -5,20 +5,44 @@
 package qctl
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
 
 	_ "github.com/chai2010/qingcloud-go/pkg/internal/glogger"
+	verpkg "github.com/chai2010/qingcloud-go/pkg/version"
 )
 
 func Main() {
 	app := cli.NewApp()
 	app.Name = "qctl"
-	app.Usage = "QingCloud SDK control"
-	app.Action = func(c *cli.Context) error {
-	  println("TODO")
-	  return nil
+	app.Usage = "Query or send control commands to the QingCloud"
+	app.Version = verpkg.Version
+
+	app.Authors = []cli.Author{
+		{Name: "ChaiShushan", Email: "chaishushan@gmail.com"},
+	}
+
+	app.CommandNotFound = func(c *cli.Context, command string) {
+		fmt.Fprintf(c.App.Writer, "ERR: command %q not found!\n", command)
+	}
+
+	app.Commands = []cli.Command{
+		cmdInstance,
+		cmdCluster,
+		cmdVolume,
+		cmdNic,
+		cmdVxnet,
+		cmdRouter,
+		cmdEip,
+		cmdSecurityGroup,
+		cmdKeyPair,
+		cmdImage,
+		cmdLoadBalancer,
+		cmdMonitor,
+		cmdSnapshot,
+		cmdJob,
 	}
 
 	app.Run(os.Args)
