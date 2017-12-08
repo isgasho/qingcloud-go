7eaaaf1aff5d5068d0e20e7a60d8369e8fe989fd
chai2010 <chaishushan@gmail.com>
2017-12-09 00:37:23 +0800

    更新代码生成插件, 新生成的代码不在依赖 pkg/request 包, 而通过 pkg/client 包代替

--- pkg/cmd/protoc-gen-qingcloud/generator/go-validator/tmpl.go
+++ pkg/cmd/protoc-gen-qingcloud/generator/go-validator/tmpl.go
@@ -33,7 +33,6 @@ import "regexp"
 
 import "github.com/chai2010/qingcloud-go/pkg/config"
 import "github.com/chai2010/qingcloud-go/pkg/logger"
-import "github.com/chai2010/qingcloud-go/pkg/request"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -43,7 +42,6 @@ var _ = math.Inf
 var _ = regexp.Match
 var _ = config.Config{}
 var _ = logger.Info
-var _ = request.Request{}
 
 `
 
