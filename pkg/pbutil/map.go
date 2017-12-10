59c5d4525631482286d92af2dcbfb6df1d9ee69c
chai2010 <chaishushan@gmail.com>
2017-12-11 00:49:19 +0800

    pkg/pbutil: 重构接口

---
+++ pkg/pbutil/map.go
@@ -0,0 +1,17 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package pbutil
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+func EncodeToMap(msg proto.Message) (m map[string]string, err error) {
+	return ProtoMessageToMap(msg)
+}
+
+func DecodeFromMap(m map[string]string, x proto.Message) error {
+	panic("TODO")
+}
