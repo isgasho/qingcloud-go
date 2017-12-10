90d51198bc466939af35ad8249cc3fd7f42646d7
chai2010 <chaishushan@gmail.com>
2017-12-11 00:02:42 +0800

    pkg/status: 新包, 用于替代 pkg/errors

---
+++ pkg/status/code.go
@@ -0,0 +1,25 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package status
+
+const (
+	ErrCode_NULL = 0
+
+	// Client
+	ErrCode_1100 = 1100
+	ErrCode_1200 = 1200
+	ErrCode_1300 = 1300
+	ErrCode_1400 = 1400
+
+	ErrCode_2100 = 2100
+	ErrCode_2400 = 2400
+	ErrCode_2500 = 2500
+
+	// Server
+	ErrCode_5000 = 5000
+	ErrCode_5100 = 5100
+	ErrCode_5200 = 5200
+	ErrCode_5300 = 5300
+)
---
+++ pkg/status/status.go
@@ -0,0 +1,64 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package status
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Status interface {
+	GetRetCode() int32 // 0 and http.StatusOK is OK
+	GetMessage() string
+}
+
+func New(code int32, msg string) Status {
+	return &pkgStatus{code: code, desc: msg}
+}
+
+func Newf(code int32, format string, a ...interface{}) Status {
+	return &pkgStatus{code: code, desc: fmt.Sprintf(format, a...)}
+}
+
+func Error(s Status) error {
+	if s == nil {
+		return nil
+	}
+	if v := s.GetRetCode(); v == 0 || v == int32(http.StatusOK) {
+		return nil
+	}
+
+	if err, ok := s.(error); ok {
+		return err
+	}
+
+	return &pkgError{
+		pkgStatus: pkgStatus{
+			code: s.GetRetCode(),
+			desc: s.GetMessage(),
+		},
+	}
+}
+
+type pkgStatus struct {
+	code int32
+	desc string
+}
+
+func (p *pkgStatus) GetRetCode() int32 {
+	return p.code
+}
+
+func (p *pkgStatus) GetMessage() string {
+	return p.desc
+}
+
+type pkgError struct {
+	pkgStatus
+}
+
+func (p *pkgError) Error() string {
+	return fmt.Sprintf("error: code = %v, desc = %v", p.code, p.desc)
+}
