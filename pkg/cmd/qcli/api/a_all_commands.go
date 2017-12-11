914f82a365ac5f6a013fe6c0ab44cbeb815915c3
chai2010 <chaishushan@gmail.com>
2017-12-12 00:21:28 +0800

    qcli: 完善配置文件加载流程

--- pkg/cmd/qcli/api/a_all_commands.go
+++ pkg/cmd/qcli/api/a_all_commands.go
@@ -8,6 +8,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"os"
+	"runtime"
+	"strings"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/urfave/cli"
@@ -17,23 +20,58 @@ import (
 
 var AllCommands []cli.Command
 
+const pkgDefaultConfig = `{
+	"api_server": "https://api.qingcloud.com/iaas/",
+	"access_key_id": "",
+	"secret_access_key": "",
+	"zone": ""
+}
+`
+
+func init() {
+	if !pkgIsUserConfigExists() {
+		pkgInstallUserConfig()
+	}
+}
+
 func pkgGetServerInfo(c *cli.Context) (p *pb.ServerInfo) {
 	if c.IsSet("config") {
 		p = pkgMustLoadConfig(c.GlobalString("config"))
+	} else {
+		p = pkgLoadConfigIgnoreAnyError(c.GlobalString("config"))
 	}
-	if c.IsSet("api_server") {
+
+	if c.IsSet("api_server") || p.GetApiServer() == "" {
 		p.ApiServer = proto.String(c.GlobalString("api_server"))
 	}
-	if c.IsSet("access_key_id") {
+	if c.IsSet("access_key_id") || p.GetAccessKeyId() == "" {
 		p.AccessKeyId = proto.String(c.GlobalString("access_key_id"))
 	}
-	if c.IsSet("secret_access_key") {
+	if c.IsSet("secret_access_key") || p.GetSecretAccessKey() == "" {
 		p.SecretAccessKey = proto.String(c.GlobalString("secret_access_key"))
 	}
-	if c.IsSet("zone") {
+	if c.IsSet("zone") || p.GetZone() == "" {
 		p.Zone = proto.String(c.GlobalString("zone"))
 	}
+
 	return
+}
+
+func pkgIsUserConfigExists() bool {
+	cfgpath := "~/.qingcloud/qcli.json"
+	if strings.HasPrefix(cfgpath, "~/") || strings.HasPrefix(cfgpath, `~\`) {
+		cfgpath = pkgGetHomePath() + cfgpath[1:]
+	}
+
+	fi, err := os.Stat(cfgpath)
+	if err != nil {
+		return false
+	}
+	if fi.IsDir() {
+		return false
+	}
+
+	return true
 }
 
 func pkgMustLoadConfig(path string) *pb.ServerInfo {
@@ -50,4 +88,44 @@ func pkgMustLoadConfig(path string) *pb.ServerInfo {
 	}
 
 	return p
+}
+
+func pkgLoadConfigIgnoreAnyError(path string) *pb.ServerInfo {
+	p := new(pb.ServerInfo)
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return p
+	}
+
+	err = json.Unmarshal(data, p)
+	if err != nil {
+		return p
+	}
+
+	return p
+}
+
+func pkgInstallUserConfig() error {
+	cfgpath := "~/.qingcloud/qcli.json"
+	if strings.HasPrefix(cfgpath, "~/") || strings.HasPrefix(cfgpath, `~\`) {
+		cfgpath = pkgGetHomePath() + cfgpath[1:]
+	}
+
+	return ioutil.WriteFile(cfgpath, []byte(pkgDefaultConfig), 0644)
+}
+
+func pkgGetHomePath() string {
+	home := os.Getenv("HOME")
+	if runtime.GOOS == "windows" {
+		home = os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+	}
+	if home == "" {
+		home = "~"
+	}
+
+	return home
 }
