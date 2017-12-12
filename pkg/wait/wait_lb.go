82679c3c200842ae2bb8614de8d062c612645177
chai2010 <chaishushan@gmail.com>
2017-12-12 23:36:02 +0800

    pkg/wait: 完善 lb 相关状态

--- pkg/wait/wait_lb.go
+++ pkg/wait/wait_lb.go
@@ -3,3 +3,46 @@
 // license that can be found in the LICENSE file.
 
 package wait
+
+import (
+	"fmt"
+	"time"
+
+	pb "github.com/chai2010/qingcloud-go/pkg/api"
+	statuspkg "github.com/chai2010/qingcloud-go/pkg/status"
+)
+
+func WaitLoadBalancerStatus(
+	server *pb.ServerInfo, lbId string,
+	status statuspkg.LoadBalancerStatus,
+	timeout time.Duration,
+) error {
+	return WaitForIntervalWorkDone(
+		fmt.Sprintf("lb:%v", lbId), timeout, func() (done bool, err error) {
+			return waitLoadBalancerStatus(server, lbId, status)
+		},
+	)
+}
+
+func waitLoadBalancerStatus(
+	server *pb.ServerInfo, lbId string,
+	expectedStatus statuspkg.LoadBalancerStatus,
+) (done bool, err error) {
+	reply, err := pb.NewLoadBalancerService(server).DescribeLoadBalancers(&pb.DescribeLoadBalancersInput{
+		Loadbalancers: []string{lbId},
+	})
+	if err != nil {
+		return false, err
+	}
+
+	if len(reply.GetLoadbalancerSet()) != 1 {
+		return false, fmt.Errorf("can not find lb [%s]", lbId)
+	}
+
+	insStatus := statuspkg.LoadBalancerStatus(reply.GetLoadbalancerSet()[0].GetStatus())
+	if insStatus == expectedStatus {
+		return true, nil
+	}
+
+	return false, nil
+}
