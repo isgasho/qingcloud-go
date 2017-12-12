73e318fafbdf0d273ca74fb1bd29e484c0038016
chai2010 <chaishushan@gmail.com>
2017-12-12 15:07:50 +0800

    更多的例子文档

--- examples_test.go
+++ examples_test.go
@@ -10,7 +10,7 @@ import (
 	sigpkg "github.com/chai2010/qingcloud-go/pkg/signature"
 )
 
-func Example_signature_Build() {
+func Example_signatureBuild() {
 	query, signature := sigpkg.Build(
 		"QYACCESSKEYIDEXAMPLE", "SECRETACCESSKEY",
 		"GET", "/iaas/", map[string]string{},
@@ -18,7 +18,47 @@ func Example_signature_Build() {
 
 	fmt.Println(query)
 	fmt.Println(signature)
+
 	// Output:
 	// access_key_id=QYACCESSKEYIDEXAMPLE&signature_method=HmacSHA256&signature_version=1&signature=O5EhQeUqTF00g59t5Pb46QPfnPMhUOAcxTWvzlnraeE%3D
 	// O5EhQeUqTF00g59t5Pb46QPfnPMhUOAcxTWvzlnraeE%3D
 }
+
+func Example_signatureGetSignatureInfo() {
+	s := "access_key_id=QYACCESSKEYIDEXAMPLE&signature_method=HmacSHA256&signature_version=1&signature=O5EhQeUqTF00g59t5Pb46QPfnPMhUOAcxTWvzlnraeE%3D"
+	pubKey, sigMethod, sigVersion, sig := sigpkg.GetSignatureInfo(s)
+
+	fmt.Println(pubKey)
+	fmt.Println(sigMethod)
+	fmt.Println(sigVersion)
+	fmt.Println(sig)
+
+	// Output:
+	// QYACCESSKEYIDEXAMPLE
+	// HmacSHA256
+	// 1
+	// O5EhQeUqTF00g59t5Pb46QPfnPMhUOAcxTWvzlnraeE=
+}
+
+func Example_signatureValidate() {
+	// TODO
+}
+
+/*
+
+func GetSignatureInfo(query string) (pubKey, sigMethod, sigVersion, sig string) {
+	m := pkgDecodeQueryToMapString(query)
+
+	pubKey = m["access_key_id"]
+	sigMethod = m["signature_method"]
+	sigVersion = m["signature_version"]
+	sig = m["signature"]
+
+	return
+}
+
+func Validate(
+	privateKeyGetter func(pubKey string) string,
+	method, urlpath, query string,
+) bool {
+*/
