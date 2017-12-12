719b317b252b26630570410b41ec9a7d19156779
chai2010 <chaishushan@gmail.com>
2017-12-12 23:28:59 +0800

    pkg/wait: 完善 instance 相关状态

--- examples_test.go
+++ examples_test.go
@@ -14,6 +14,7 @@ import (
 	pb "github.com/chai2010/qingcloud-go/pkg/api"
 	"github.com/chai2010/qingcloud-go/pkg/pbutil"
 	sigpkg "github.com/chai2010/qingcloud-go/pkg/signature"
+	statuspkg "github.com/chai2010/qingcloud-go/pkg/status"
 	"github.com/chai2010/qingcloud-go/pkg/wait"
 )
 
@@ -74,6 +75,7 @@ func Example_helloSDK() {
 	s, _ := pbutil.EncodeJsonIndent(reply)
 	fmt.Println(s)
 }
+
 func Example_waitJob() {
 	// import pb "github.com/chai2010/qingcloud-go/pkg/api"
 	// import "github.com/chai2010/qingcloud-go/pkg/wait"
@@ -95,4 +97,32 @@ func Example_waitJob() {
 	}
 
 	fmt.Println("job done")
+}
+
+func Example_waitInstance() {
+	// import pb "github.com/chai2010/qingcloud-go/pkg/api"
+	// import statuspkg "github.com/chai2010/qingcloud-go/pkg/status"
+	// import "github.com/chai2010/qingcloud-go/pkg/wait"
+
+	qc := pb.NewInstanceService(&pb.ServerInfo{
+		AccessKeyId:     proto.String("QYACCESSKEYIDEXAMPLE"),
+		SecretAccessKey: proto.String("SECRETACCESSKEY"),
+		Zone:            proto.String("pek3a"),
+	})
+
+	reply, err := qc.RunInstances(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = wait.WaitInstanceStatus(
+		qc.ServerInfo, reply.GetInstances()[0],
+		statuspkg.InstanceStatus_Running,
+		time.Second*5,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("instance running")
 }
--- pkg/wait/wait_ins.go
+++ pkg/wait/wait_ins.go
@@ -3,3 +3,75 @@
 // license that can be found in the LICENSE file.
 
 package wait
+
+import (
+	"fmt"
+	"time"
+
+	pb "github.com/chai2010/qingcloud-go/pkg/api"
+	statuspkg "github.com/chai2010/qingcloud-go/pkg/status"
+)
+
+func WaitInstanceStatus(
+	server *pb.ServerInfo, insId string,
+	status statuspkg.InstanceStatus,
+	timeout time.Duration,
+) error {
+	return WaitForIntervalWorkDone(
+		fmt.Sprintf("ins:%v", insId), timeout, func() (done bool, err error) {
+			return waitInstanceStatus(server, insId, status)
+		},
+	)
+}
+
+func WaitInstanceNetwork(
+	server *pb.ServerInfo, insId string,
+	timeout time.Duration,
+) error {
+	return WaitForIntervalWorkDone(
+		fmt.Sprintf("ins:%v", insId), timeout, func() (done bool, err error) {
+			return waitInstanceNetwork(server, insId)
+		},
+	)
+}
+
+func waitInstanceStatus(
+	server *pb.ServerInfo, insId string,
+	expectedStatus statuspkg.InstanceStatus,
+) (done bool, err error) {
+	reply, err := pb.NewInstanceService(server).DescribeInstances(&pb.DescribeInstancesInput{
+		Instances: []string{insId},
+	})
+	if err != nil {
+		return false, err
+	}
+
+	if len(reply.GetInstanceSet()) != 1 {
+		return false, fmt.Errorf("can not find instance [%s]", insId)
+	}
+
+	insStatus := statuspkg.InstanceStatus(reply.GetInstanceSet()[0].GetStatus())
+	if insStatus == expectedStatus {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func waitInstanceNetwork(
+	server *pb.ServerInfo, insId string,
+) (done bool, err error) {
+	reply, err := pb.NewInstanceService(server).DescribeInstances(&pb.DescribeInstancesInput{
+		Instances: []string{insId},
+	})
+	if err != nil {
+		return false, err
+	}
+
+	if len(reply.GetInstanceSet()) != 1 {
+		return false, fmt.Errorf("can not find instance [%s]", insId)
+	}
+
+	vxnets := reply.GetInstanceSet()[0].GetVxnets()
+	return len(vxnets) > 0 && vxnets[0].GetPrivateIp() != "", nil
+}
