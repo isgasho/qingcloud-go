2f5356d7e4fb38cd958396a6b8d38f173c5c33a6
chai2010 <chaishushan@gmail.com>
2017-12-14 17:26:06 +0800

    qcli: import lua package

---
+++ pkg/cmd/qcli/cmd_luafile.go
@@ -0,0 +1,11 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package qcli
+
+import (
+	_ "github.com/yuin/gopher-lua"
+)
+
+// config in ./qclifile.lua
