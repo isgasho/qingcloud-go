4fec78745b48b80c2c3df30f2366edd3be890c74
chai2010 <chaishushan@gmail.com>
2017-12-21 14:48:44 +0800

    test: 新目录, 用于以后放置集成测试脚本

---
+++ test/main.lua
@@ -0,0 +1,9 @@
+-- Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+-- Use of this source code is governed by a Apache
+-- license that can be found in the LICENSE file.
+
+module(..., package.seeall)
+
+function testMain()
+	print("testMain: TODO")
+end
