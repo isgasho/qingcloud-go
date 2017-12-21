c96c8652cc6f9e4b8c8c248a99103ca938c25d9d
chai2010 <chaishushan@gmail.com>
2017-12-21 10:40:16 +0800

    pkg/gopher-lua/getopt: 命令行解析包

---
+++ pkg/gopher-lua/getopt/hello.go
@@ -0,0 +1,49 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"github.com/yuin/gopher-lua"
+
+	getopt "github.com/chai2010/qingcloud-go/pkg/gopher-lua/getopt"
+)
+
+func main() {
+	L := lua.NewState()
+	defer L.Close()
+
+	getopt.Preload(L)
+
+	if err := L.DoString(code); err != nil {
+		panic(err)
+	}
+}
+
+const code = `
+local getopt = require("getopt")
+
+local arg = {"-aone", "-btwo", "-c=three", "--file=foo.lua", "abc", "123"}
+local opts, arg = getopt( arg, "abc" )
+
+for k, v in pairs(opts) do
+  print( k, v )
+end
+
+for k, v in pairs(arg) do
+  print( k, v )
+end
+
+--[[
+output:
+	a       one
+	b       two
+	c       =three
+	file    foo.lua
+	1       abc
+	2       123
+--]]
+`
