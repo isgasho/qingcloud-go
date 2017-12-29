d8a5129a8c1e071c0820cd98733f96b682045042
chai2010 <chaishushan@gmail.com>
2017-12-29 15:16:40 +0800

    fix build

--- pkg/cmd/qcli/cmd_lua_init.go
+++ pkg/cmd/qcli/cmd_lua_init.go
@@ -15,8 +15,8 @@ import (
 	lua_socket "github.com/BixData/gluasocket"
 	lua_http "github.com/cjoudrey/gluahttp"
 	lua_url "github.com/cjoudrey/gluaurl"
-	lua_json "github.com/layeh/gopher-json"
-	lua_lfs "github.com/layeh/gopher-lfs"
+	lua_json "layeh.com/gopher-json"
+	lua_lfs "layeh.com/gopher-lfs"
 
 	lua_getopt "github.com/chai2010/qingcloud-go/pkg/gopher-lua/getopt"
 	lua_inspect "github.com/chai2010/qingcloud-go/pkg/gopher-lua/inspect"
